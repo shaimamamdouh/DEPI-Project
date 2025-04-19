af2afe5b7c6ad3f9a62a174678c7e702784b56f9
emanGamal <eman54gamal@gmail.com>
2025-04-19 12:08:35 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Readio());
+}
+
+class Readio extends StatelessWidget {
+  const Readio({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
