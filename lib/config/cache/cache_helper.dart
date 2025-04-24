b4e2a5e7897545a2fdfd491463431b904fb2637d
emanGamal <eman54gamal@gmail.com>
2025-04-24 17:07:06 +0200

    secure storage

--- lib/config/cache/cache_helper.dart
+++ lib/config/cache/cache_helper.dart
@@ -1,7 +1,10 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CacheHelper {
   static late SharedPreferences _sharedPreferences; //declare the variable
+  static final secureStorage = FlutterSecureStorage();
+
 
   static init() async {
     _sharedPreferences =
@@ -9,7 +12,7 @@ class CacheHelper {
   }
 
 
-   //save data to shared preferences
+  //save data to shared preferences
   static Future<bool> setData({
     required String key,
     required dynamic value,
@@ -23,24 +26,43 @@ class CacheHelper {
     }
     return false;
   }
-   
 
 
-    //get data from shared preferences
+  //get data from shared preferences
   static dynamic getData({required String key}) {
     return _sharedPreferences.get(key);
   }
 
-
-
-    //remove data from shared preferences
+  //remove data from shared preferences
   static Future<bool> removeData({required String key}) async {
     return await _sharedPreferences.remove(key);
   }
 
-
-     //clear all data from shared preferences
+  //clear all data from shared preferences
   static Future<bool> clearData() async {
     return await _sharedPreferences.clear();
+  }
+
+  //save data to secure storage
+  static Future<void> setSecureData({
+    required String key,
+    required String value,
+  }) async {
+    await secureStorage.write(key: key, value: value);
+  }
+
+  //get data from secure storage
+  static Future<String?> getSecureData({required String key}) async {
+    return await secureStorage.read(key: key);
+  }
+
+  //remove data from secure storage
+  static Future<void> removeSecureData({required String key}) async {
+    await secureStorage.delete(key: key);
+  }
+
+  //clear all data from secure storage
+  static Future<void> clearSecureData() async {
+    await secureStorage.deleteAll();
   }
 }
